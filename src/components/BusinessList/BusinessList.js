e963a07465dc68d33fb0c88f0d3799f5031d8cba
Martin Leroux <tintin_151@hotmail.com>
2017-10-07 17:04:38 -0400

    Pass down information between components.

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,10 +7,9 @@ class BusinessList extends React.Component {
   render(){
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {this.props.businesses.map(function(business){
+          return <Business business={business} />
+        })}
       </div>
     )
   }
